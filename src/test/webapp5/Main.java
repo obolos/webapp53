0ff96e17a554a5554b7d43e8601547d5b62879aa
obolos <mycontaktperson@gmail.com>
2016-03-19 12:59:51 +0400

    first new class

---
+++ src/test/webapp5/Main.java
@@ -0,0 +1,10 @@
+package test.webapp5;
+
+/**
+ * Created by Jdev on 19.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
